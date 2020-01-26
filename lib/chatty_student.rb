2a2ab4b01d2dbb5e2c7a7eabb1d3729a1b2bf727
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-01-26 18:23:06 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -8,8 +8,13 @@ class ChattyStudent
     puts "Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
   end 
 
+<<<<<<< HEAD
   def raise_hand
     phrase = "Pick me!\n"
     puts phrase * 10
+=======
+  def raise_hand 
+    puts "Pick me!\nPickme!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPickme!\n"
+>>>>>>> 8c265603b91c4e88e8812fc40c207c27de2a6aec
   end 
 end 
